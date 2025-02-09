086fc58b681887a32cba8e09cfb3dea422f44b6f
taralalukasz <taralalukasz123@gmail.com>
2025-02-09 11:16:27 +0100

    applied strategy pattern for file extraction

--- backend/modules/extract/extract.py
+++ backend/modules/extract/extract.py
@@ -1,23 +1,34 @@
-from pathlib import Path
-from typing import Optional
-
+from abc import ABC, abstractmethod
 import pandas as pd
 
-class FileExtractor:
-    def __init__(self):
+class ExtractStrategy(ABC):
+    @abstractmethod    
+    def extract(path_to_file: str):
         pass
 
-    def extract_file(self, file_path: str):
-        data_file_path = Path(file_path)
-        file_extension = data_file_path.suffix
-        df = None
-        if file_extension.lower() == ".csv":
-            print("loading csv...")
-            df = pd.read_csv(file_path)
-        elif file_extension.lower() in [".xls", ".xlsx"]:
-            print("loading excel...")
-            df = pd.read_excel(file_path)
-        else :
-            raise TypeError(file_extension)
-        return df
-    
+class ExtractCsvStrategy(ExtractStrategy):
+ 
+    def extract(path_to_file: str):
+        print("loading csv...")
+        return pd.read_csv(path_to_file)
+
+class ExtractExcelStrategy(ExtractStrategy):
+
+    def extract(path_to_file: str):
+        print("loading excel...")
+        return pd.read_excel(path_to_file)
+
+class ExtractPdfStrategy(ExtractStrategy):
+ 
+    def extract(path_to_file: str):
+        raise NotImplementedError("PDF not implemented yet")
+
+class ExtractContext():
+    def __init__(self, strategy: ExtractStrategy):
+        self.strategy = strategy
+
+    def chooseStrategy(self, strategy: ExtractStrategy):
+        self.strategy = strategy
+
+    def extract(self, path_to_file: str) -> pd.DataFrame:
+        return self.strategy.extract(path_to_file)
--- backend/modules/transform/transform.py
+++ backend/modules/transform/transform.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import Optional
+from pathlib import Path
+import pandas as pd
+
+import modules.extract.extract as extract
 
 class EtlHandler(ABC):
     def __init__(self, next_handler: Optional["EtlHandler"] = None):
@@ -9,9 +13,33 @@ class EtlHandler(ABC):
     def process(self, data):
         pass
 
-    def next(self, data):
+    def next(self, data: pd.DataFrame):
         return self.next_handler.process() if self.next_handler != None else data
 
 # TODO - add Extractor Handler
+class ExtractHandler(EtlHandler):
+    def __init__(self, path_to_file: str, next_handler: Optional["EtlHandler"] = None):
+        self.path_to_file = path_to_file
+        self.next_handler = next_handler
+
+    def process(self, data = None):
+        data_file_path = Path(self.path_to_file)
+        file_extension = data_file_path.suffix()
+        strat = self._chooseStrategy(file_extension)
+        extract_context = extract.ExtractContext(strat)
+        df = extract_context.extract(self.path_to_file)
+        self.next(df)
+
+    def _chooseStrategy(file_extension: str) -> extract.ExtractStrategy:
+        if file_extension.lower() == ".csv":
+            return extract.ExtractCsvStrategy()
+        elif file_extension.lower() in [".xls", ".xlsx"]:
+            return extract.ExtractExcelStrategy()
+        elif file_extension.lower() in [".pdf"]:
+            return extract.ExtractPdfStrategy
+        else :
+            raise TypeError(file_extension)
+
+
 # TODO - add Transformation Handler
 # TODO - add Persistance Handler
