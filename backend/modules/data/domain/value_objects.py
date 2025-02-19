49f9f779f12a8c7124d5a62b243179299985e62b
taralalukasz <taralalukasz123@gmail.com>
2025-02-19 10:03:07 +0100

    used pandera DataFrameModel

--- backend/modules/data/domain/value_objects.py
+++ backend/modules/data/domain/value_objects.py
@@ -2,14 +2,15 @@ import pandera as pa
 from pandera.typing import Series
 
 
-class InputData(pa.SchemaModel):
+
+class InputData(pa.DataFrameModel):
     name: Series[str]
     surname: Series[str]
     age: Series[int] = pa.Field(ge=0)
     is_satisfied: Series[bool]
 
 
-class OutputData(pa.SchemaModel):
+class OutputData(pa.DataFrameModel):
     full_name: Series[str]
     age: Series[int] = pa.Field(ge=0)
     is_satisfied: Series[bool]
