3802d32fe5f98e2c7ec68aeb9357d88838f24170
mateuszglowinski <mglowinski93@gmail.com>
2025-02-24 22:05:00 +0100

    Fix mypy

--- backend/modules/extract/services/commands/commands.py
+++ backend/modules/extract/services/commands/commands.py
@@ -2,6 +2,7 @@ import logging
 
 import pandas as pd
 import pandera as pa
+from pandera.typing.pandas import DataFrame
 
 from ....common.domain.exceptions import DataValidationException
 from ....data.domain import value_objects as data_value_objects
@@ -11,7 +12,9 @@ from ..strategies import AbstractExtraction, choose_strategy
 logger = logging.getLogger(__name__)
 
 
-def extract(command: domain_commands.ExtractData) -> data_value_objects.InputData:
+def extract(
+    command: domain_commands.ExtractData,
+) -> DataFrame[data_value_objects.InputData]:
     logger.info(f"Started data extraction from {str(command.file_path.name)}.")
     if not command.file_path.exists():
         raise FileNotFoundError(
@@ -20,11 +23,10 @@ def extract(command: domain_commands.ExtractData) -> data_value_objects.InputDat
 
     read_strategy: AbstractExtraction = choose_strategy(command.file_path.suffix)()
 
-    df: pd.DataFrame = read_strategy.read(command.file_path)
+    data: pd.DataFrame = read_strategy.read(command.file_path)
 
     try:
-        validated_data = data_value_objects.InputData.validate(df)
-
+        validated_data = data_value_objects.InputData.validate(data)
     except pa.errors.SchemaError:
         logger.info(f"Invalid input data in file {str(command.file_path.name)}.")
         raise DataValidationException(
@@ -32,4 +34,5 @@ def extract(command: domain_commands.ExtractData) -> data_value_objects.InputDat
         )
 
     logger.info(f"Successfully extracted dataset from {str(command.file_path.name)}.")
-    return validated_data
+
+    return validated_data  # type: ignore[return-value]
