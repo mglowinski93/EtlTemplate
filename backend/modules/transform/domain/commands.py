ef10d24d1d815ffd3c67a16b9d83a0795ec62c94
taralalukasz <taralalukasz123@gmail.com>
2025-02-26 11:05:45 +0100

    used typed dataframe as transform command input data

--- backend/modules/transform/domain/commands.py
+++ backend/modules/transform/domain/commands.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 
+from pandera.typing.pandas import DataFrame
+
 from ...common.domain import commands as common_commands
 from ...data.domain import value_objects as data_value_objects
 
 
 @dataclass(frozen=True)
 class TransformData(common_commands.DomainCommand):
-    data: data_value_objects.InputData
+    data: DataFrame[data_value_objects.InputData]
