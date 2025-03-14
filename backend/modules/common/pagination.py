a7aa7641dbd241fee1e4b8faf196abc7de297f42
taralalukasz <taralalukasz123@gmail.com>
2025-03-14 12:24:03 +0100

    added pagination common class

---
+++ backend/modules/common/pagination.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+
+PAGINATION_DEFAULT_OFFSET = 0
+PAGINATION_DEFAULT_LIMIT = 100
+
+
+@dataclass
+class Pagination:
+    offset: int
+    records_per_page: int
+
+    def __post_init__(self):
+        try:
+            self.offset = int(self.offset)
+        except ValueError:
+            raise ValueError("Offset must be an integer.")
+        if self.offset < 0:
+            raise ValueError("Offset must be greater than or equal to 0.")
+
+        try:
+            self.records_per_page = int(self.records_per_page)
+        except ValueError:
+            raise ValueError("Offset must be an integer.")
+        if self.records_per_page < 0:
+            raise ValueError("Records per page must be greater than or equal to 0.")
