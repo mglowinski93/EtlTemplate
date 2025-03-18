3ae728de50aacfaf165e836f2c93a3ff413ed60b
taralalukasz <taralalukasz123@gmail.com>
2025-03-18 08:06:37 +0100

    add filter, ordering and pagination to query repository

--- backend/infrastructures/apps/load/views/views.py
+++ backend/infrastructures/apps/load/views/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from modules.common import pagination as pagination_dtos
 
 from modules.load.services.queries import ports as query_ports
 
@@ -24,7 +25,11 @@ class LoadViewSet(
     ):
         logger.info("Listing all datasets...")
         #TODO: fix load view set by defining filter, pagination and ordering and add them here
-        output_data, count = query_data_repository.list()
+        output_data, count = query_data_repository.list(
+            filters = query_ports.OutputDataFilters(),
+        ordering = query_ports.OutputDataOrdering(),
+        pagination = pagination_dtos.Pagination(pagination_dtos.PAGINATION_DEFAULT_OFFSET, pagination_dtos.PAGINATION_DEFAULT_LIMIT)
+        )
         return Response(
             data={
                 "count": count,
