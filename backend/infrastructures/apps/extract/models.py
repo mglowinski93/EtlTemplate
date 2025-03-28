7989f03179df8184b645724fca9654959bad34a1
taralalukasz <taralalukasz123@gmail.com>
2025-03-28 18:41:46 +0100

    added db extract history model

---
+++ backend/infrastructures/apps/extract/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+ 
+from ..common import models as common_models
+ 
+
+class ExtractHistory(common_models.AutomaticallyTimestampedModel):
+    input_file_name = models.CharField(max_length=256)
+    saved_file_name = models.CharField(max_length=256)
+ 
+
+    class Meta:
+        db_table = "extract_history"
