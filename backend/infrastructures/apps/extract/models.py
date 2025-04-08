04183e9c33e5f06eced0a7ca0a5afb6a15ee4dad
taralalukasz <taralalukasz123@gmail.com>
2025-04-08 18:49:19 +0200

    regenerated migrations

--- backend/infrastructures/apps/extract/models.py
+++ backend/infrastructures/apps/extract/models.py
@@ -13,5 +13,6 @@ class ExtractHistory(common_models.ManuallyTimestampedModel):
         editable=False,
         verbose_name=_("ID"),
     )
-    input_file_name = models.CharField(max_length=255)
-    saved_file_name = models.CharField(max_length=255)
+    input_file_name = models.CharField(max_length=255, help_text=_("Name of file retrieved file."))
+    saved_file_name = models.CharField(max_length=255, help_text=("Name from the file as it was saved."))
+    
