4f138591711d48c92245d17f9f54e901836ca6cb
taralalukasz <taralalukasz123@gmail.com>
2025-04-10 19:10:49 +0200

    extract data query get method exceptions tested

--- backend/tests/integrated/load/adapters/test_repositories.py
+++ backend/tests/integrated/load/adapters/test_repositories.py
@@ -1,4 +1,4 @@
-from modules.load.domain import ports 
+from modules.load.domain import ports , value_objects
 from modules.load.services.queries.ports import repositories as query_repositories
 from modules.load.services.queries import ports as query_ports
 from modules.common import pagination as pagination_dtos
@@ -80,23 +80,41 @@ def test_django_data_query_repository_list_method_raises_custom_exception_on_dja
         pagination=pagination_dtos.Pagination(offset=pagination_dtos.PAGINATION_DEFAULT_OFFSET, records_per_page=pagination_dtos.PAGINATION_DEFAULT_LIMIT)
     )
 
+#todo fix it
+# def test_django_data_query_repository_get_method_returns_detailed_record(
+#     test_django_data_domain_repository: ports.AbstractDataDomainRepository,
+#     test_django_data_query_repository: query_repositories.AbstractDataQueryRepository,
+# ):
+#     # Given
+#     data_entity = [transform_value_objects.OutputData(full_name="John Snow", age=10, is_satisfied= True)]    
+#     test_django_data_domain_repository.create(data_entity)
 
-def test_django_data_query_repository_list_method_queries_all_records(
-    test_django_data_domain_repository: ports.AbstractDataDomainRepository,
+#     # When
+#     result = test_django_data_query_repository.get() #todo add param id
+
+#     #Then
+#     assert all(data.full_name == result.full_name for data in data_entity)
+
+
+def test_django_data_query_repository_get_method_raises_custom_exception_when_data_not_found(
     test_django_data_query_repository: query_repositories.AbstractDataQueryRepository,
 ):
     # Given
-    data_entity = [transform_value_objects.OutputData(full_name="John Snow", age=10, is_satisfied= True)]    
-    test_django_data_domain_repository.create(data_entity)
+    # When and Then
+    with pytest.raises(common_exceptions.DataDoesNotExist):
+        test_django_data_query_repository.get(data_id=value_objects.DataId.new()) 
 
-    # When
-    results, count = test_django_data_query_repository.list(
-        filters=query_ports.DataFilters(),
-        ordering=query_ports.DataOrdering(),
-        pagination=pagination_dtos.Pagination(offset=pagination_dtos.PAGINATION_DEFAULT_OFFSET, records_per_page=pagination_dtos.PAGINATION_DEFAULT_LIMIT)
+def test_django_data_query_repository_get_method_raises_custom_exception_on_django_exception(
+    mocker: MockFixture,
+    test_django_data_query_repository: query_repositories.AbstractDataQueryRepository,
+):
+    # Given
+    side_effect = DatabaseError
+    mocker.patch(
+        "infrastructures.apps.load.models.Data.objects.get",
+        side_effect=side_effect,
     )
 
-    #Then
-    assert count == 1
-    assert all(models.Data.objects.filter(data__contains={"full_name": data.full_name}).exists() for data in results)
-
+    # When and Then
+    with pytest.raises(common_exceptions.DatabaseError):
+        test_django_data_query_repository.get(data_id=value_objects.DataId.new()) 
