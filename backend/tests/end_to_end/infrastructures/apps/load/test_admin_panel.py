742a8d8aaf6c199a836a9bd261529983eb59ad0b
taralalukasz <taralalukasz123@gmail.com>
2025-04-18 14:08:12 +0200

    admin panel delete test

--- backend/tests/end_to_end/infrastructures/apps/load/test_admin_panel.py
+++ backend/tests/end_to_end/infrastructures/apps/load/test_admin_panel.py
@@ -84,8 +84,50 @@ def test_editing_data(
     assert data.data["full_name"] == new_full_name
 
 
+def test_delete_data(
+    unauthenticated_client: APIClientData,
+    data_admin_panel: admin.DataAdmin,
+):
+    # Given
+    client = unauthenticated_client.client
+
+    data: Data = model_factories.DataFactory.create()
+
+    form = admin.DataAdminForm(
+        data={
+            "data": {
+                "full_name": data.data["full_name"],
+                "age": data.data["age"],
+                "is_satisfied": data.data["is_satisfied"],
+            },
+        }
+    )
+
+    with transaction.atomic():
+        form.is_valid()
+        assert not form.errors
+    change = False
+    
+    data_admin_panel.save_model(
+        request=HttpRequest(), obj=data, form=form, change=change
+    )
+
+    # When
+    data_admin_panel.delete_model(
+        request=HttpRequest(), obj=data,
+    )
+
+    #Then
+    response = client.get(
+        get_url(
+            path_name="load-detail",
+            path_params={"pk": data.id},
+        )
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
 # todo planned tests
-# test test_delete_data_queryset
-# test_data_export_via_event_tab_to_excel_file
+# test_data_export_via_data_tab_to_excel_file
 # test_admin_form_raises_validation_error_when_age_is_not_a_number
+# test_admin_form_raises_validation_error_when_name_is_not_a_string
 # test_admin_form_raises_validation_error_when_is_satisfied_is_not_bool
